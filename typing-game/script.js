dd94e54a67a1bc468e0fb27a2de68abad2454092
Achim Grether <a.grether@hotmail.com>
2021-12-08 12:44:45 +0100

    save the last scores in the typing game

--- typing-game/script.js
+++ typing-game/script.js
@@ -1,4 +1,8 @@
 const quotes = [
+    'abc def',
+];
+/*
+const quotes = [
     'When you have eliminated the impossible, whatever remains, however improbable, must be the truth.',
     'There is nothing more deceptive than an obvious fact.',
     'I ought to know by this time that when a fact appears to be opposed to a long train of deductions it invariably proves to be capable of bearing some other interpretation.',
@@ -7,10 +11,14 @@ const quotes = [
     'Nothing clears up a case so much as stating it to another person.',
     'Education never ends, Watson. It is a series of lessons, with the greatest for the last.',
 ];
+*/
 
 let words = [];
 let wordIndex = 0;
 let startTime = Date.now();
+let scoreID = 0;
+let myStorage = window.sessionStorage;
+myStorage.clear();
 
 const quoteElement = document.getElementById('quote');
 const messageElement = document.getElementById('message');
@@ -46,36 +54,51 @@ document.getElementById('start').addEventListener('click', () => {
     startTime = new Date().getTime();
   });
 
-  typedValueElement.addEventListener('input', () => {
+typedValueElement.addEventListener('input', () => {
     // Get the current word
     const currentWord = words[wordIndex];
     // get the current value
     const typedValue = typedValueElement.value;
-  
+
     if (typedValue === currentWord && wordIndex === words.length - 1) {
-      // end of sentence
-      // Display success
-      const elapsedTime = new Date().getTime() - startTime;
-      const message = `CONGRATULATIONS! You finished in ${elapsedTime / 1000} seconds.`;
-      messageElement.innerText = message;
+        // end of sentence
+        // Display success
+        const elapsedTime = new Date().getTime() - startTime;
+        const message = `CONGRATULATIONS! You finished in ${elapsedTime / 1000} seconds.`;
+        myStorage.setItem(scoreID, elapsedTime.toString());
+        upgradeLeaderBoard();
+        scoreID++;
+        messageElement.innerText = message;
     } else if (typedValue.endsWith(' ') && typedValue.trim() === currentWord) {
-      // end of word
-      // clear the typedValueElement for the new word
-      typedValueElement.value = '';
-      // move to the next word
-      wordIndex++;
-      // reset the class name for all elements in quote
-      for (const wordElement of quoteElement.childNodes) {
+        // end of word
+        // clear the typedValueElement for the new word
+        typedValueElement.value = '';
+        // move to the next word
+        wordIndex++;
+        // reset the class name for all elements in quote
+        for (const wordElement of quoteElement.childNodes) {
         wordElement.className = '';
-      }
-      // highlight the new word
-      quoteElement.childNodes[wordIndex].className = 'highlight';
+        }
+        // highlight the new word
+        quoteElement.childNodes[wordIndex].className = 'highlight';
     } else if (currentWord.startsWith(typedValue)) {
-      // currently correct
-      // highlight the next word
-      typedValueElement.className = '';
+        // currently correct
+        // highlight the next word
+        typedValueElement.className = '';
     } else {
-      // error state
-      typedValueElement.className = 'error';
+        // error state
+        typedValueElement.className = 'error';
     }
-  });
+});
+
+function upgradeLeaderBoard() {
+    let tableRef = document.getElementById("highscore");
+    let newRow = tableRef.insertRow(scoreID + 1);
+    let newCell = newRow.insertCell(0);
+    let newCell2 = newRow.insertCell(1);
+    let newText = document.createTextNode(scoreID);
+    let newText2 = document.createTextNode(myStorage.getItem(scoreID));
+    newCell.appendChild(newText);
+    newCell2.appendChild(newText2);
+}
+    
