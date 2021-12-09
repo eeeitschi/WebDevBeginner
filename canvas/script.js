4acea984ff8afadf6421ae9d76716e33531967c6
Achim Grether <a.grether@hotmail.com>
2021-12-09 09:07:57 +0100

    completed Web-Dev-For-Beginner course

---
+++ canvas/script.js
@@ -0,0 +1,17 @@
+// draws a red rectangle
+//1. get the canvas reference
+canvas = document.getElementById("myCanvas");
+
+//2. set the context to 2D to draw basic shapes
+ctx = canvas.getContext("2d");
+
+//3. fill it with the color red
+ctx.fillStyle = 'red';
+
+//4. and draw a rectangle with these parameters, setting location and size
+ctx.fillRect(0,0, 200, 200) // x,y,width, height
+ctx.fillStyle = 'orange';
+
+ctx.beginPath();
+ctx.arc(300, 300, 60, 0, 2 * Math.PI);
+ctx.stroke();
